d75168e72d444957657409906004ffaa767e8e97
elduke717 <spacematcha@proton.me>
2023-07-21 21:34:02 -0400

    add form view and functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import BarGraph from './BarGraph.js';
 import PieChart from './PieChart.js';
 import Footer from './Footer';
+import ApplicationFormView from './ApplicationFormView.js';
 
 function App() {
+  const [formData, setFormData] = useState({
+    applications: 0,
+    phoneScreens: 0,
+    inPersonInterviews: 0,
+    offers: 0,
+  });
+
+  const handleFormSubmit = (formData) => {
+    setFormData(formData);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>The Job Hunt</h1>
         <div className='link-container'>
-        <a className='link' href="/">Bar Graph</a>
-        <a className='link' href="/pie-chart">Where to Apply</a>
+          <Link className='link' to="/">Application Metrics</Link>
+          <Link className='link' to="/pie-chart">Where to Apply</Link>
+          <Link className='link' to="/application-form">Application Form</Link>
         </div>
       </header>
       <Router>
         <Routes>
-          <Route path="/" element={<BarGraph />} />
-          <Route path="/targets" element={<PieChart />} />
+          <Route path="/" element={<BarGraph formData={formData} />} />
+          <Route path="/pie-chart" element={<PieChart />} />
+          <Route path="/application-form" element={<ApplicationFormView onSubmit={handleFormSubmit} />} />
         </Routes>
       </Router>
       <Footer />
     </div>
-    
   );
 }
 
 export default App;
+
+
 
---
+++ src/ApplicationFormView.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+function ApplicationFormView({ onSubmit }) {
+  const [formData, setFormData] = useState({
+    applications: 0,
+    phoneScreens: 0,
+    inPersonInterviews: 0,
+    offers: 0,
+  });
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const newFormData = {
+      applications: parseInt(event.target.elements.applications.value),
+      phoneScreens: parseInt(event.target.elements.phoneScreens.value),
+      inPersonInterviews: parseInt(event.target.elements.inPersonInterviews.value),
+      offers: parseInt(event.target.elements.offers.value),
+    };
+    setFormData(newFormData);
+    onSubmit(newFormData);
+  };
+
+  return (
+    <div>
+      <h1>Application Form</h1>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label>Applications:</label>
+          <input type="number" name="applications" />
+        </div>
+        <div>
+          <label>Phone Screens:</label>
+          <input type="number" name="phoneScreens" />
+        </div>
+        <div>
+          <label>In-Person Interviews:</label>
+          <input type="number" name="inPersonInterviews" />
+        </div>
+        <div>
+          <label>Offers:</label>
+          <input type="number" name="offers" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default ApplicationFormView;
