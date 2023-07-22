2882bac39d2c0eea25eec61627d32763059d4372
elduke717 <spacematcha@proton.me>
2023-07-21 21:37:34 -0400

    separate form view removed

--- src/App.js
+++ src/App.js
@@ -1,45 +1,30 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BarGraph from './BarGraph.js';
 import PieChart from './PieChart.js';
 import Footer from './Footer';
-import ApplicationFormView from './ApplicationFormView.js';
 
 function App() {
-  const [formData, setFormData] = useState({
-    applications: 0,
-    phoneScreens: 0,
-    inPersonInterviews: 0,
-    offers: 0,
-  });
-
-  const handleFormSubmit = (formData) => {
-    setFormData(formData);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>The Job Hunt</h1>
         <div className='link-container'>
-          <Link className='link' to="/">Application Metrics</Link>
-          <Link className='link' to="/pie-chart">Where to Apply</Link>
-          <Link className='link' to="/application-form">Application Form</Link>
+        <a className='link' href="/">Bar Graph</a>
+        <a className='link' href="/pie-chart">Where to Apply</a>
         </div>
       </header>
       <Router>
         <Routes>
-          <Route path="/" element={<BarGraph formData={formData} />} />
-          <Route path="/pie-chart" element={<PieChart />} />
-          <Route path="/application-form" element={<ApplicationFormView onSubmit={handleFormSubmit} />} />
+          <Route path="/" element={<BarGraph />} />
+          <Route path="/targets" element={<PieChart />} />
         </Routes>
       </Router>
       <Footer />
     </div>
+    
   );
 }
 
 export default App;
-
-
 
