636963b8b7bbb1ec7adc2bf81915965256794454
elduke717 <spacematcha@proton.me>
2023-08-01 10:10:56 -0400

    add more data to graph

--- src/BarGraph.js
+++ src/BarGraph.js
@@ -7,11 +7,12 @@ import { AxisLeft, AxisBottom } from '@visx/axis';
 // Our data...
 const data = [
   { type: 'Applications', value: 22 },
+  { type: 'E-mail', value: 2 },
   { type: 'Phone Screens', value: 0 },
   { type: 'Interviews', value: 0 },
-  { type: 'Offers', value: 0 },
-  { type: 'Acceptances', value: 0 },
   { type: 'Rejections', value: 6 },
+  { type: 'Acceptances', value: 0 },
+  { type: 'Offers', value: 0 }
 ];
 
 // Define the graph dimensions and margins
